74caf4ce02d067d037fbfd42b397be3af31fb678
Drishya Puthanpurakal Manohar <pmanohar.drishya2@gmail.com>
2024-06-07 19:36:26 -0400

    production cahges

--- src/app/services/job.service.ts
+++ src/app/services/job.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
-  private apiUrl = 'http://localhost:4200'; 
+  private apiUrl = environment.apiUrl; 
+
  
   constructor(private http: HttpClient) { }
 
   //All jobs API
   getAllJobs(): Observable<[]> {
+    console.log(this.apiUrl)
     return this.http.get<[]>(`${this.apiUrl}/jobs`);
   }
 
